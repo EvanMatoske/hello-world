30ad5cfe33958fcc2893cf2b90cf91cadd675588
Evan Matoske <evanmatoske@gmail.com>
2016-04-02 12:38:03 -0400

    Editing
    Adding stuff

---
+++ HelloCS290.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Hello World git
+//
+//  Created by Evan Matoske on 4/2/16.
+//  Copyright © 2016 Evan Matoske. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, CS 290!\n");
+    return 0;
+}
